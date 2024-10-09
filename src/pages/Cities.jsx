516ee5ac6a0320f79418ee41e5c6eb0b32e99284
Manuel <manuelgarcia8295@gmail.com>
2024-10-09 00:21:01 -0500

    99

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -12,8 +12,8 @@ export default function Cities() {
     <>
     <div className=' inset-0 flex flex-col items-center justify-center text-white  bg-opacity-50"'>
 
-  <h1 className="text-9xl font-bold mb-4">Ooops...</h1>
-  <h2 className='text-8xl text-center my-10'>
+  <h1 className="text-7xl font-bold mb-4">Ooops...</h1>
+  <h2 className='text-6xl text-center my-10'>
   Page under construction
             </h2>
     <img src="https://www.lucushost.com/blog/wp-content/uploads/2018/11/como-crear-una-pagina-en-construccion.png" alt="construccion" />
